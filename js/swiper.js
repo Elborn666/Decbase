51c0d2af120cfb38230808db9c94498855cc7457
Elborn666 <elborn18@gmail.com>
2024-04-01 10:49:23 +0300

    Initial commit

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.testimonials__button-right',
+      prevEl: '.testimonials__button-left',
+    },
+  });
